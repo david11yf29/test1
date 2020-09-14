7b3fda3e381fce0d776c57537b2f71dc45af2cf0
david11yf29 <david11yf29@gmail.com>
2020-09-14 23:29:23 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+import Homepage from './homepage.component.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  )
 }
 
 export default App;
