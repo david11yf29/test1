387d26c7e40601c27b16b073f0f713f385b0402b
david11yf29 <david11yf29@gmail.com>
2020-09-15 22:19:47 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
 
 import './App.css';
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <Route exact path="/" component={Homepage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
     </div>
   )
 }
