3f2055f2963e887c94f7949f6d30638b98c3b1ee
david11yf29 <david11yf29@gmail.com>
2020-09-15 20:14:32 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
 
 import './App.css';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Route exact path="/" component={Homepage} />
       <Route path='/shop' component={ShopPage} />
     </div>
