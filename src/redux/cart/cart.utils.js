2a221f7a8fb636350998952bc6951466a4e72d49
david11yf29 <david11yf29@gmail.com>
2020-09-19 10:23:09 +0800

    updated

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,6 +3,10 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   // 原本就在裡面
   if (existingCartItem) {
-    return 
+    return cartItems.map(cartItem => cartItem.id === cartItemToAdd.id ? 
+    { ...cartItem, quantity: cartItem.quantity + 1 }
+    : cartItem )
   }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
