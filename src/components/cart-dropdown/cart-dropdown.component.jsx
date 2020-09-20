57506efe96aa6f1cb5d73ce2fc2e6a04d1bf66dd
david11yf29 <david11yf29@gmail.com>
2020-09-20 22:21:25 +0800

    updated

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors.js';
-import { withRouter } from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
 import './cart-dropdown.styles.scss';
 
 
 const CartDropdown = (props) => {
-  const { cartItems } = props;
+  const { cartItems, history, dispatch } = props;
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -20,7 +21,10 @@ const CartDropdown = (props) => {
           : <span className="empty-massage">Your cart is empty</span>
         }
       </div>
-      <CustomButton onClick={() => props.history.push("/checkout")}>GO TO CHECKOUT</CustomButton>
+      <CustomButton onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartHidden());
+      }}>GO TO CHECKOUT</CustomButton>
     </div>
   )
 }
